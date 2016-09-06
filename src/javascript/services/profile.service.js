e47c2c4992c8b2374348afdede9dd7da577e1167
Gary Stevens <gary.stevens@work-club.com>
2016-09-06 16:16:07 +0100

    updating content

--- src/javascript/services/profile.service.js
+++ src/javascript/services/profile.service.js
@@ -7,65 +7,100 @@
 
         function ProfileService($q){
 
+			var d = new Date;
+
+			Number.prototype.padLeft = function(base,chr){
+    			var  len = (String(base || 10).length - String(this).length)+1;
+    			return len > 0? new Array(len).join(chr || '0')+this : this;
+			}
+
+    		var dformat = [(d.getMonth()+1).padLeft(),
+               d.getDate().padLeft(),
+               d.getFullYear()].join('/') +' ' +
+              [d.getHours().padLeft(),
+               d.getMinutes().padLeft(),
+               d.getSeconds().padLeft()].join(':');
+
             var searchTerms = {
                 'about': {
 					'responses': [
-						'test',
 						' ',
-						' /home '
+						'For the last 3 years I have specialised in leading technical ',
+						'teams in delivery, whilst also helping creative teams refine and hone digital ideas. ',
+						'I am passionate about technology and enjoy the thrill of ',
+						'the impossible challenge. I believe in championing great work and the craft required to ',
+						'deliver it. I am a driven, motivated ',
+						'person who enjoys continually pushing my skills and learning. ',
+						'I also enjoy sharing this knowledge and enjoy talking at meet-ups whenever possible. ',
+						' ',
+						' /root '
 					]
 				},
 				'start': {
 					'responses': [
-						'welcome',
+						' ',
+						'login: usr anon on ' + dformat + ' to term',
+						'Counting objects: done.',
+						'Compressing objects: 100% (5/5), done.',
+						'Writing objects: 100% (5/5), 1.44 KiB | 0 bytes/s, done.',
+						'Resolving Host......',
+						'RESOLVED',
+						' ',
+						'> Welcome',
 						'type "help" for more',
 						'information',
 						'.......',
-						'PROCESSING',
 						'END',
 						' ',
-						' /home '
+						' /root '
 					]
 				},
                 'work': {
 					'responses': [
-						'one',
-						'two',
-						'three',
-						'...four',
-						'five',
-						'six',
-						'seven',
-						'eight...',
 						' ',
-						' /home '
+						'> Current ',
+						'Tech Lead @ Havas London',
+						' ',
+						'http://www.chivas.com/en/the-venture',
+						'http://www.mclaren.com/formula1/',
+						'https://www.ballantinesgolfclub.com/en/',
+						'Durex',
+						' ',
+						'> Previous',
+						'Developer @ Specialmoves',
+						'Wacom',
+						'Rolex',
+						' ',
+						'/root '
 					]
 				},
                 'contact': {
 					'responses': [
+						' ',
 						'email: garymstevens@gmail.com',
 						'twitter: @garymstevens',
 						'github: https://github.com/garystevens',
+						'linkedIn: https://www.linkedin.com/in/gary-stevens-36958622',
 						' ',
 						' /home '
 					]
 				},
 				'help': {
 					'responses': [
-						'you can use the following commands...',
+						'> You can use the following commands...',
 						' ',
 						'work',
 						'contact',
 						'about',
 						' ',
-						' /home '
+						' /root '
 					]
 				},
 				'cd ../': {
 					'responses': [
-						'nice try',
+						'> Nice try',
 						' ',
-						' /home '
+						' /root '
 					]
 				}
             };
